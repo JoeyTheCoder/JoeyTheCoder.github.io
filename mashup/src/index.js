57508a937e30af1be6237395e43b33df69c09b22
Joel Sahli <joel.sahli@outlook.com>
2019-02-02 17:50:49 +0100

    Initial commit

--- mashup/src/index.js
+++ mashup/src/index.js
@@ -1,16 +1,8 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-/**
- * @author: Joel Sahli
- * @version: 2.0
- */
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
