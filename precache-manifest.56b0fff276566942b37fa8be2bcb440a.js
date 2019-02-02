987c87bac48142657c91b46fb586e2e353be5c51
Joel Sahli <joel.sahli@outlook.com>
2019-02-02 17:23:06 +0100

    Updates

---
+++ precache-manifest.56b0fff276566942b37fa8be2bcb440a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "0a2aa8dd094cf978b65f",
+    "url": "/react/static/js/main.0a2aa8dd.chunk.js"
+  },
+  {
+    "revision": "0148014f8f40fab6133c",
+    "url": "/react/static/js/1.0148014f.chunk.js"
+  },
+  {
+    "revision": "0a2aa8dd094cf978b65f",
+    "url": "/react/static/css/main.00a61d91.chunk.css"
+  },
+  {
+    "revision": "96ba80680197713bbab17668aae6c585",
+    "url": "/react/index.html"
+  }
+];
